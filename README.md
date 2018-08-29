253dc2361b3a0cc166fbe50a5b8d44888a77c245
James Weakley <jameswillisweakley@gmail.com>
2018-08-29 20:44:53 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Snowflake Rules Based Grant Management
+Simplifies the management of Snowflake grants, enables strict control and stops privileges falling into the wrong hands.
+
+Allows security permissions in [Snowflake](https://www.snowflake.net) to be managed via rules that support wildcards and apply across all databases.
+
+## How it works
+### Default Snowflake behaviour
+Snowflake provides the ability to grant collections of users (known as roles) privileges on either 
+specific objects, or all objects of a specific type, e.g.
+* grant select on all tables in schema mydb.myschema to role analyst;
+* grant select,insert,update,delete on table customer to role developer;
+
+Nested roles are supported, which allows a hierarchy to be created.
+
+###What this script provides in addition
+This script provides a slightly different approach. Rather than using nested hierarchies to reduce 
+the number of GRANTs required, a rule file is provided which supports wildcards.
+
+This means a very terse and readable configuration file can generate a very large number of GRANTs.
+
+Because it's a structured file, it can reside in version control and can be subject to change management
+enforcement (e.g. pull requests with mandatory reviews).
+
+By default, the script will also remove any privileges not specified by the file, so any direct adding of 
+privileges into the database are revoked at each run.
+
+###Restrictions
+
+Currently, only [schemaObjectPrivileges](https://docs.snowflake.net/manuals/sql-reference/sql/grant-privilege.html) are 
+supported, and only for tables and views.
+
+The script also does not generate grant statements that include "ALL" clauses, it's only specific objects. This means that it only acts on current tables and views, and must be re-ran each time tables or views are added.
+
+For this reason, it is recommended that it is triggered automatically after any schema change operations.
+
+
+## Execution
+With python 3 installed, the script can be ran like so:
+```
+pip install --upgrade snowflake-connector-python
+python apply_permissions.py -a $SNOWFLAKE_ACCOUNT -u $SNOWFLAKE_USER -r $SNOWFLAKE_ROLE -w $SNOWFLAKE_WAREHOUSE --snowflake-region $SNOWFLAKE_REGION 
+```
+it is expected that the environment variable SNOWSQL_PWD be set prior to calling the script, you should make this available to your build agent in some secure fashion.
+
+You'll need to map between the branch name and the target environment name, e.g. 
+
+Or if you prefer docker, set the environment variables and run like so:
+```
+docker run -it --rm \
+  -v "$PWD":/usr/src/applypermissions \
+  -w /usr/src/applypermissions \
+  -e SNOWFLAKE_ACCOUNT \
+  -e SNOWFLAKE_USER \
+  -e SNOWFLAKE_ROLE \
+  -e SNOWFLAKE_WAREHOUSE \
+  -e SNOWFLAKE_REGION \
+  -e SNOWFLAKE_REGION \
+  -e SNOWSQL_PWD \
+  --name apply-permissions \
+  python:3 /bin/bash -c "pip install --upgrade snowflake-connector-python && python apply_permissions.py -a $SNOWFLAKE_ACCOUNT -u $SNOWFLAKE_USER -r $SNOWFLAKE_ROLE -w $SNOWFLAKE_WAREHOUSE --snowflake-region $SNOWFLAKE_REGION"
+```
+
+###File specification
+See below example, where each schemaObjectPrivileges child contains:
+* **Purpose**: A plain english description of the rule
+* **Role**: The name of the Snowflake role to grant permission to
+* **Databases**: Matches the databases to apply to. Format is [unix filename pattern](https://docs.python.org/2/library/fnmatch.html).
+* **Schemas**: Matches the schemas to apply the rule to (uses the [Snowflake LIKE format](https://docs.snowflake.net/manuals/sql-reference/functions/like.html))
+* **Tables**: Matches the tables to apply the rule to (uses the [Snowflake LIKE format](https://docs.snowflake.net/manuals/sql-reference/functions/like.html)). Leave empty to not apply to any tables.
+* **Views**: Matches the views to apply the rule to (uses the [Snowflake LIKE format](https://docs.snowflake.net/manuals/sql-reference/functions/like.html))Leave empty to not apply to any views.
+* **Privileges**: The [schemaObjectPrivileges](https://docs.snowflake.net/manuals/sql-reference/sql/grant-privilege.html) to grant. (Must be possible or it is ignored, e.g. UPDATE applied to a view)
+
+
+####Example
+```
+{
+    "schemaObjectPrivileges": [
+        {
+            "Purpose": "Grant data warehouse developers the complete freedom they need in the development environment",
+            "Role": "DataWarehouseDevelopers",
+            "Databases": "*_DEV",
+            "Schemas": "%",
+            "Tables": "%",
+            "Views": "%",
+            "Privileges": ["SELECT","INSERT","UPDATE","DELETE","TRUNCATE"]
+        },
+        {
+            "Purpose": "Grant reporting users the ability to select from any view in all production databases and schemas",
+            "Role": "ReportViewers",
+            "Databases": "*_PROD",
+            "Schemas": "%",
+            "Tables": "",
+            "Views": "%",
+            "Privileges": ["SELECT"]
+        },
+        {
+            "Purpose": "Grant customer service users the ability to select only from Customer-related views in production only",
+            "Role": "CustomerService",
+            "Databases": "*_PROD",
+            "Schemas": "%",
+            "Tables": "CUSTOMER%",
+            "Views": "%",
+            "Privileges": ["SELECT"]
+        }
+    ]
+}
+```
+
+## Notes
+
+**Be very careful running this script if you have existing privileges that have already applied!**
+
+To be safe, use the ```--grant-statements-file``` and ```--revoke-statements-file``` parameters to output the change scripts to a file, rather than apply them directly to the database.
+
+This is a community-developed script, not an official Snowflake offering. It comes with no support.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ apply_permissions.py
@@ -0,0 +1,227 @@
+import os
+import snowflake.connector
+import argparse
+import json
+import fnmatch
+
+def apply_permissions(snowflake_account, snowflake_user, snowflake_role, snowflake_warehouse, snowflake_region,
+                      permissions_file, grant_statements_file, revoke_statements_file, verbose):
+    if os.environ["SNOWSQL_PWD"] is None:
+        raise ValueError("The SNOWSQL_PWD environment variable has not been defined")
+    os.environ["SNOWFLAKE_ACCOUNT"] = snowflake_account
+    os.environ["SNOWFLAKE_USER"] = snowflake_user
+    os.environ["SNOWFLAKE_ROLE"] = snowflake_role
+    os.environ["SNOWFLAKE_WAREHOUSE"] = snowflake_warehouse
+    os.environ["SNOWFLAKE_REGION"] = snowflake_region
+    os.environ["SNOWFLAKE_AUTHENTICATOR"] = 'snowflake'
+
+    all_databases = fetch_databases(verbose)
+    print("databases found: {0}".format(all_databases))
+    with open(permissions_file) as f:
+        permissions_json = json.load(f)
+
+    # for each database, gather all existing grant statements
+    all_defined_privileges={}
+    existing_database_grants={}
+    for database in all_databases:
+        existing_database_grants[database] = generate_existing_privileges_sql(database, verbose)
+        all_defined_privileges[database]=set()
+        if verbose:
+            print("Statements to grant existing privileges in database '{0}': {1}".format(database,existing_database_grants[database]))
+
+
+    # iterate through the privilege rules defined in the file.
+    # for each rule, generate all grant statements required to bring it into effect,
+    # storing them grouped by database. Using sets will eliminate any rule overlap and prevent duplicate grant statements
+    for schema_object_privilege in permissions_json["schemaObjectPrivileges"]:
+        print("Applying rule {0}".format(schema_object_privilege["Purpose"]))
+        selected_databases = [x for x in all_databases if fnmatch.fnmatch(x,schema_object_privilege["Databases"])]
+        print(selected_databases)
+        for database in selected_databases:
+            print("Applying rule to database {0}".format(database))
+            print("Generating SQL script to determine existing privileges")
+
+            grant_privileges_sql_statements = generate_grant_privileges_sql(database,
+                                                                 schema_object_privilege["Role"],
+                                                                 schema_object_privilege["Schemas"],
+                                                                 schema_object_privilege["Tables"],
+                                                                 schema_object_privilege["Views"],
+                                                                 schema_object_privilege["Privileges"],
+                                                                 verbose)
+
+            all_defined_privileges[database]=all_defined_privileges[database].union(grant_privileges_sql_statements)
+
+    # for each database, produce lists of both missing and superfluous statements (the latter being REVOKEs),
+    # such that running them all should bring the database in sync with the rules file
+    all_missing_privileges={}
+    all_superfluous_privileges={}
+
+    all_missing_privileges_string = "BEGIN TRANSACTION;\n"
+    all_superfluous_privileges_string = "BEGIN TRANSACTION;\n"
+    for database in all_databases:
+        all_missing_privileges[database] = (list(set(all_defined_privileges[database]) - set(existing_database_grants[database])))
+        all_superfluous_privileges[database] = list(set(existing_database_grants[database]) - set(all_defined_privileges[database]))
+
+        all_missing_privileges_string = "{0}\n// ------------ Database {1}\n{2}".format(
+            all_missing_privileges_string,
+            database,
+            ";\n".join(all_missing_privileges[database]))
+
+        if len(all_missing_privileges[database]) > 0:
+            all_missing_privileges_string = "{0};\n".format(all_missing_privileges_string)
+
+        all_superfluous_privileges_string = "{0}\n// ------------ Database {1}\n{2}\n".format(
+            all_superfluous_privileges_string,
+            database,
+            ";\n".join(all_superfluous_privileges[database]).replace('GRANT ','REVOKE ').replace(' TO ROLE ',' FROM ROLE '))
+
+        if len(all_superfluous_privileges[database]) > 0:
+            all_superfluous_privileges_string = "{0};\n".format(all_superfluous_privileges_string)
+
+        if verbose:
+            print("For database {0}".format(database))
+            print("  Missing privileges: {0}".format(all_missing_privileges[database]))
+            print("  Superfluous privileges: {0}".format(all_superfluous_privileges[database]))
+
+    all_missing_privileges_string = "{0}\nCOMMIT;\n".format(all_missing_privileges_string)
+    all_superfluous_privileges_string = "{0}\nCOMMIT;\n".format(all_superfluous_privileges_string)
+
+    if len(all_missing_privileges.values()) == 0:
+        print("No missing privileges, so no file output or execution required")
+    else:
+        if grant_statements_file:
+            print("Writing GRANT statements to file {0}".format(grant_statements_file))
+            grant_file = open(grant_statements_file,'w')
+            grant_file.write(all_missing_privileges_string)
+            grant_file.close()
+        else:
+            print("Executing GRANT statements in database")
+            execute_snowflake_query(database, None, all_missing_privileges_string, verbose)
+
+    if len(all_superfluous_privileges.values()) == 0:
+        print("No superfluous privileges, so no file output or execution required")
+    else:
+        if revoke_statements_file:
+            print("Writing REVOKE statements to file {0}".format(revoke_statements_file))
+            grant_file = open(revoke_statements_file,'w')
+            grant_file.write(all_superfluous_privileges_string)
+            grant_file.close()
+        else:
+            print("Executing REVOKE statements in database")
+            execute_snowflake_query(database, None, all_superfluous_privileges_string, verbose)
+
+
+    print("Completed successfully")
+
+
+def execute_snowflake_query(snowflake_database, snowflake_schema, query, verbose):
+    con = snowflake.connector.connect(
+        user=os.environ["SNOWFLAKE_USER"],
+        account=os.environ["SNOWFLAKE_ACCOUNT"],
+        role=os.environ["SNOWFLAKE_ROLE"],
+        warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
+        database=snowflake_database,
+        schema=snowflake_schema,
+        region=os.environ["SNOWFLAKE_REGION"],
+        authenticator=os.environ["SNOWFLAKE_AUTHENTICATOR"],
+        password=os.environ["SNOWSQL_PWD"]
+    )
+    if verbose:
+        print("SQL query: %s" % query)
+    try:
+        return con.execute_string(query)
+    finally:
+        con.close()
+
+
+def fetch_databases(verbose):
+    query = "SELECT DATABASE_NAME FROM UTIL_DB.INFORMATION_SCHEMA.DATABASES dbs WHERE dbs.DATABASE_NAME NOT IN ('UTIL_DB')"
+    results = execute_snowflake_query('UTIL_DB', None, query, verbose)
+    databases = []
+    for cursor in results:
+        for row in cursor:
+            databases.append(row[0])
+    return databases
+
+def fetch_roles(snowflake_database, snowflake_schema, verbose):
+    query = "SHOW ROLES"
+    results = execute_snowflake_query(snowflake_database, snowflake_schema, query, verbose)
+    roles = []
+    for cursor in results:
+        for row in cursor:
+            if row[0] != 'Statement executed successfully.':
+                roles.append(row[0])
+    return roles
+
+def generate_existing_privileges_sql(snowflake_database, verbose):
+    query = """SELECT 'GRANT '||PRIVILEGE_TYPE||' ON '||REPLACE (tab.TABLE_TYPE,'BASE ','')||' '||OBJECT_CATALOG||'.'||OBJECT_SCHEMA||'.'||OBJECT_NAME||' TO ROLE "'||GRANTEE||'"' AS Privileges
+    FROM {0}.INFORMATION_SCHEMA.OBJECT_PRIVILEGES priv
+    JOIN {0}.INFORMATION_SCHEMA.TABLES tab ON priv.OBJECT_CATALOG=tab.TABLE_CATALOG and priv.OBJECT_SCHEMA=tab.TABLE_SCHEMA and priv.OBJECT_NAME=tab.TABLE_NAME
+    WHERE OBJECT_CATALOG IS NOT NULL
+    AND PRIVILEGE_TYPE != 'OWNERSHIP'""".format(snowflake_database)
+    results = execute_snowflake_query(snowflake_database, None, query, verbose)
+    existing_privileges = []
+    for cursor in results:
+        for row in cursor:
+            if row[0] != 'Statement executed successfully.':
+                existing_privileges.append(row[0])
+    return existing_privileges
+
+def all_possible_privileges():
+    return ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'TRUNCATE', 'REFERENCES']
+
+def all_possible_privileges_clause():
+    return (" union ".join(["select '{0}' as name".format(privileges) for privileges in all_possible_privileges()]))
+
+def generate_grant_privileges_sql(database_name, grantee_role,schemas, tables, views, privileges, verbose):
+    query = """USE DATABASE {0};
+    SELECT 'GRANT '||privs.name||' ON TABLE {0}.'||schms.SCHEMA_NAME||'.'||tbls.TABLE_NAME||' TO ROLE \"{1}\"' AS Statement
+    FROM INFORMATION_SCHEMA.SCHEMATA schms
+    JOIN INFORMATION_SCHEMA.TABLES tbls ON (tbls.TABLE_SCHEMA = schms.SCHEMA_NAME)
+    RIGHT OUTER JOIN ({5}) privs
+    WHERE schms.SCHEMA_NAME LIKE '{2}'
+    AND tbls.TABLE_NAME LIKE '{3}'
+    AND schms.SCHEMA_NAME NOT IN ('INFORMATION_SCHEMA')
+    AND privs.name IN ({6})
+    UNION
+    SELECT 'GRANT '||privs.name||' ON VIEW {0}.'||schms.SCHEMA_NAME||'.'||vws.TABLE_NAME||' TO ROLE \"{1}\"' AS Statement
+    FROM INFORMATION_SCHEMA.SCHEMATA schms
+    JOIN INFORMATION_SCHEMA.VIEWS vws ON (vws.TABLE_SCHEMA = schms.SCHEMA_NAME)
+    RIGHT OUTER JOIN (select 'SELECT' as name) privs
+    WHERE schms.SCHEMA_NAME LIKE '{2}'
+    AND vws.TABLE_NAME LIKE '{4}'
+    AND schms.SCHEMA_NAME NOT IN ('INFORMATION_SCHEMA')
+    AND privs.name IN ({6})""".format(database_name,grantee_role,schemas, tables, views, all_possible_privileges_clause(),",".join(["'{0}'".format(privilege) for privilege in privileges]))
+    results = execute_snowflake_query(database_name, None, query, verbose)
+    grant_privileges_sql = []
+    for cursor in results:
+        for row in cursor:
+            if row[0] != 'Statement executed successfully.':
+                grant_privileges_sql.append(row[0])
+    return grant_privileges_sql
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Apply a set of permission rules to a Snowflake account")
+    parser.add_argument('-a', '--snowflake-account', type=str, help='The name of the environment (e.g. dev,test,prod)',
+                        required=True)
+    parser.add_argument('-u', '--snowflake-user', type=str, help='The name of the snowflake user (e.g. deployer)',
+                        required=True)
+    parser.add_argument('-r', '--snowflake-role', type=str, help='The name of the role to use (e.g. DEPLOYER_ROLE)',
+                        required=True)
+    parser.add_argument('-w', '--snowflake-warehouse', type=str,
+                        help='The name of the warehouse to use (e.g. DEPLOYER_WAREHOUSE)', required=True)
+    parser.add_argument('--snowflake-region', type=str, help='The name of the snowflake region (e.g. ap-southeast-2)',
+                        required=True)
+    parser.add_argument('-p', '--permissions-file', default="Permissions.json", type=str,
+                        help='The file containing the permission definitions')
+    parser.add_argument('-g', '--grant-statements-file', default=None, type=str,
+                        help='The name of a file to output the GRANT statements to, instead of applying them automatically')
+    parser.add_argument('-e', '--revoke-statements-file', default=None, type=str,
+                        help='The name of a file to output the REVOKE statements to. instead of applying them automatically')
+    parser.add_argument('-v', '--verbose', type=bool, default=False)
+    args = parser.parse_args()
+
+
+apply_permissions(args.snowflake_account, args.snowflake_user, args.snowflake_role, args.snowflake_warehouse,
+                  args.snowflake_region, args.permissions_file,args.grant_statements_file,args.revoke_statements_file, args.verbose)
